e83ee28ee02bcadb97376aa48a100b9d2c1285e7
Hidden-Bug-0x1 <gwprielipp@gmail.com>
2021-06-16 11:35:00 -0800

    Added the pixel v1.0.0

---
+++ include/pixel.hpp
@@ -0,0 +1,15 @@
+#ifndef PIXEL_HPP
+#define PIXEL_HPP
+
+#include <stdlib.h>
+
+class Pixel {
+public:
+  int32_t red;
+  int32_t blue;
+  int32_t green;
+  int32_t alpha;
+  Pixel(int32_t r, int32_t g, int32_t b, int32_t a); // constructor 
+};
+
+#endif
---
+++ src/pixel.cpp
@@ -0,0 +1,4 @@
+#include "../include/pixel.hpp"
+
+Pixel::Pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+: red(r), green(g), blue(b), alpha(a) {}
